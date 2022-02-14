c2b4d41e8ce2527adc32f3c2b07200427db9a997
unknown <your email>
2022-02-14 17:37:29 -0500

    updated files, EOD checkpoint

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import CarSerializer
+from .models import Car
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def cars_list(request):
 
+    if request.method == 'GET':
+        cars = Car.objects.all()
+        serializer = CarSerializer(cars, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
 
-    return Response('ok')
